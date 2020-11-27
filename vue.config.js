7e48961a8b3bd07341a6338d5860e81ef7f4900d
zhaoyuncai <275231202@qq.com>
2020-11-27 16:05:36 +0800

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
-module.exports={
-  resolve:{
-    alias:{
-      'assets':'@/assets',
-      'common':'@/common',
-      'components':'@/components',
-      'network':'@/network',
-      'views':'@/views',
+module.exports = {
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
     }
   }
+
 }
+
+
